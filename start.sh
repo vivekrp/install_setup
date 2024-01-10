d41626f806fac342fbac73c799ae8deb94b248db
Vivek Rp <me@vivekrp.com>
2024-01-10 21:39:34 +0530

    some changes

--- start.sh
+++ start.sh
@@ -58,7 +58,10 @@ while [[ "$#" -gt 0 ]]; do
     DOPPLER_CONFIG="$2"
     shift
     ;;
-  *) usage ;;
+  *)
+    echo "Unrecognized argument: $1"
+    usage
+    ;;
   esac
   shift
 done
@@ -74,7 +77,7 @@ check_required_args() {
   [ -z "$DOPPLER_CONFIG" ] && missing_args+=("--doppler-config CONFIG")
   if [ ${#missing_args[@]} -ne 0 ]; then
     echo "Missing required arguments: ${missing_args[*]}"
-
+    usage
   fi
 }
 check_required_args
